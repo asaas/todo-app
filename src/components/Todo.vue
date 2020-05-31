050c1bf1cd1c9e566386c02e3acac44f7e92e314
tatsuya-hasebe <tatsuya.hasebe@a-saas.com>
2020-05-31 22:06:43 +0900

    add template for todo app

---
+++ src/components/Todo.vue
@@ -0,0 +1,72 @@
+<template lang="html">
+  <div class="todo">
+    <div class="filter">
+      <select>
+        <option value="all">all</option>
+        <option value="all">active</option>
+        <option value="all">completed</option>
+      </select>
+    </div>
+    <div class="spacer" />
+    <table class="todo-list">
+      <tr>
+        <td>
+          Do the dishes
+        </td>
+        <td>
+          <button>toggle</button>
+        </td>
+        <td>
+          <button>delete</button>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Take out trash
+        </td>
+        <td>
+          <button>toggle</button>
+        </td>
+        <td>
+          <button>delete</button>
+        </td>
+      </tr>
+      <tr>
+        <td class="completed">
+          Finish doing laundry
+        </td>
+        <td>
+          <button>toggle</button>
+        </td>
+        <td>
+          <button>delete</button>
+        </td>
+      </tr>
+    </table>
+    <div class="spacer" />
+    <div class="new-todo-form">
+      <input type="text" value="" />
+      <div class="spacer" />
+      <button>add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css" scoped>
+* {
+  font-size: 16px;
+  outline: none;
+}
+.spacer {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+}
+.completed {
+  text-decoration: line-through;
+}
+</style>
