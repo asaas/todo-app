61aacc5578d90666ae23e7a9787e24696320f86f
tatsuya-hasebe <tatsuya.hasebe@a-saas.com>
2020-05-31 22:40:27 +0900

    implement todo app

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,59 +1,118 @@
 <template lang="html">
   <div class="todo">
     <div class="filter">
-      <select>
+      <select v-model="visibilityFilter">
         <option value="all">all</option>
-        <option value="all">active</option>
-        <option value="all">completed</option>
+        <option value="active">active</option>
+        <option value="completed">completed</option>
       </select>
     </div>
     <div class="spacer" />
     <table class="todo-list">
-      <tr>
-        <td>
-          Do the dishes
+      <tr v-for="todo in filteredTodos" :key="todo.id">
+        <td :class="{ completed: todo.completed }">
+          {{ todo.title }}
         </td>
         <td>
-          <button>toggle</button>
+          <button @click="onToggleButtonClick(todo.id)">
+            toggle
+          </button>
         </td>
         <td>
-          <button>delete</button>
-        </td>
-      </tr>
-      <tr>
-        <td>
-          Take out trash
-        </td>
-        <td>
-          <button>toggle</button>
-        </td>
-        <td>
-          <button>delete</button>
-        </td>
-      </tr>
-      <tr>
-        <td class="completed">
-          Finish doing laundry
-        </td>
-        <td>
-          <button>toggle</button>
-        </td>
-        <td>
-          <button>delete</button>
+          <button @click="onDeleteButtonClick(todo.id)">
+            delete
+          </button>
         </td>
       </tr>
     </table>
     <div class="spacer" />
     <div class="new-todo-form">
-      <input type="text" value="" />
+      <input type="text" v-model="newTodoTitle" />
       <div class="spacer" />
-      <button>add</button>
+      <button @click="onNewTodoAddButtonClick">add</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      visibilityFilter: 'all',
+      todos: [
+        {
+          id: 1,
+          title: "Do the dishes",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Take out trash",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Finish doing laundry",
+          completed: true
+        }
+      ],
+      newTodoTitle: '',
+      newId: 4
+    }
+  },
+
+  computed: {
+    filteredTodos() {
+      const { visibilityFilter, todos } = this
+
+      switch (visibilityFilter) {
+        case 'all':
+          return todos
+        case 'active':
+          return todos.filter(todo => !todo.completed)
+        case 'completed':
+          return todos.filter(todo => todo.completed)
+        default:
+          return todos
+      }
+    }
+  },
+
+  methods: {
+    onNewTodoAddButtonClick() {
+      const { todos, newTodoTitle, newId } = this
+
+      this.todos = [
+        ...todos,
+        {
+          id: newId,
+          title: newTodoTitle,
+          completed: false
+        }
+      ]
+
+      this.newId = newId + 1
+
+      this.newTodoTitle = ''
+    },
+
+    onToggleButtonClick(id) {
+      const { todos } = this
+
+      this.todos = todos.map(todo => (
+        todo.id === id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      ))
+    },
+
+    onDeleteButtonClick(id) {
+      const { todos } = this
+
+      this.todos = todos.filter(todo => todo.id !== id)
+    }
+  }
+}
 </script>
 
 <style lang="css" scoped>
